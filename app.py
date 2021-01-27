99e951a3edfe56439068a2b9c89a16ba23180bc1
v1enna <19vienna20@gmail.com>
2021-01-27 17:31:59 +0100

    Delete junk

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, get_template_attribute, current_app
-from board import Board
-from board import Score
+from flask import Flask, url_for, render_template, get_template_attribute, current_app
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('app.html', board = Board(25, 25), score = Score())
+    return render_template('app.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
